6028ec0e3cee60bea843ccfa638024ecede3f6fa
Yuhan Liu <yliu1021@outlook.com>
2021-10-21 23:46:37 -0700

    Updated engine so that floats do no print extraneous trailing 0's or decimal points

--- cas/repl/engine.ml
+++ cas/repl/engine.ml
@@ -1,5 +1,17 @@
 open Base
 
+let float_to_string x =
+  let float_chars = Printf.sprintf "%.15f" x |> String.to_list in
+  let rec remove_prefix_zeros = function
+    | [] -> []
+    | '0' :: rem -> remove_prefix_zeros rem
+    | '.' :: rem -> rem
+    | num -> num
+  in
+  let rev_parsed_float = List.rev float_chars |> remove_prefix_zeros in
+  List.rev rev_parsed_float |> String.of_char_list
+;;
+
 let eval repl_state str =
   match Tokenizer.tokenize ~repl_state str with
   | Result.Error pos ->
@@ -14,5 +26,5 @@ let eval repl_state str =
         , String.concat [ "error: failed to convert token at pos "; Int.to_string pos ] )
       | Result.Ok expr_tree ->
         let res = Eval.to_float expr_tree in
-        repl_state, Float.to_string res))
+        repl_state, float_to_string res))
 ;;
